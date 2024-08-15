aadb78c712979c85e1ee35e536f4ec59b8410a6c
Stone Werner <stonewerner@berkeley.edu>
2024-08-15 13:35:26 -0700

    variant picker shows size instead of name, works

--- src/components/VariantPicker.tsx
+++ src/components/VariantPicker.tsx
@@ -1,14 +1,28 @@
 const VariantPicker = ({ variants, ...props }) => {
-  if (variants.length === (0 || 1)) return null;
+  console.log('Variants received by VariantPicker:', variants);
+  if (variants.length === (0)) return null;
+
+
+  if (variants.length === 1) {
+    return (
+      <select
+        {...props}
+        className="form-select appearance-none w-full relative mb-3 sm:mb-0 flex-grow sm:mr-3 pl-3 py-2 bg-white border border-gray-300 focus:border-gray-500 shadow-sm text-gray-500 text-sm focus:outline-none focus:text-gray-900 rounded ring-0 focus:ring-0"
+        disabled
+      >
+        <option>{variants[0].size}</option>
+      </select>
+    );
+  }
 
   return (
     <select
       {...props}
       className="form-select appearance-none w-full relative mb-3 sm:mb-0 flex-grow sm:mr-3 pl-3 py-2 bg-white border border-gray-300 focus:border-gray-500 shadow-sm text-gray-500 text-sm focus:outline-none focus:text-gray-900 rounded ring-0 focus:ring-0"
     >
-      {variants.map(({ external_id, name }) => (
+      {variants.map(({ external_id, size }) => (
         <option key={external_id} value={external_id}>
-          {name}
+          {size}
         </option>
       ))}
     </select>
